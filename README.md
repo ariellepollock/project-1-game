bc9bf193f08be46be4df50b9a2cf810b457279ad
ariellepollock <ariellecpollock@gmail.com>
2023-11-29 08:24:58 -0600

    add story to readme and start initial pseudo code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # project-1-game
+
+FIND ALL ITEMS BEFORE TIME RUNS OUT!!
+This game functions just like ispy, the twist is that you only get one clue at a time and you're racing against the clock. Help Milo find all the items he dropped in the forest and gather them for him. If you make good time then you win!
+
---
+++ js/main.js
@@ -0,0 +1,10 @@
+// read the directions and press start to see the first clue and start the timer
+// read the first clue
+// look for the item that belongs to the clue
+// use the hint button if stuck (hint will provide more directions/details on where to find the item)
+// when the item is found, click on it
+// when the item is clicked a border will appear around the item to signal it's been found
+// when the mouse is released from the click the item will fill in the gray silhouette next to its clue
+// repeat steps 2-7 until all items have been found
+// win message if all items have been found before the timer runs out
+// better luck next time if timer runs out before all items have been found
